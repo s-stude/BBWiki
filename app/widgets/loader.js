289bca6a3d13769c179751f22393fb06aab1351b
Akim Khalilov <Akim.Khalilov@gmail.com>
2014-04-23 17:28:45 +0400

    $.ajax instead of text.get to load the resources. Removing 404 page for now.

--- app/widgets/loader.js
+++ app/widgets/loader.js
@@ -1,34 +1,32 @@
 define(function (require) {
 
-    var text = require('text');
+    var $ = require('jQuery');
     var urlDecoder = require('urlDecoder');
-
-    var cachedNext,
-        cachedHref;
 
     var source404 = '# Sorry, 404...\n ' +
         'This page ($URL$) was not found. Please be careful with URLs. ([go to Index](/))';
 
     return {
-        onError: function (error) {
-                cachedNext(source404.replace('$URL$',urlDecoder.toRealDelimiters(cachedHref)));
-        },
-
         load: function (href, next) {
-
-            cachedNext = next;
-            cachedHref = href;
 
             href = urlDecoder.prependWithSubdomain(href);
 
             var decodedUrl = urlDecoder.toRealDelimiters(href);
             console.log('Loading: ', decodedUrl);
 
-            text.get(decodedUrl, function (source) {
+            var $ajax = $.ajax({
+                type: 'GET',
+                url: decodedUrl,
+                cache: false
+            });
 
+            $ajax.success(function(source){
                 next(source);
+            });
 
-            }, this.onError);
+            $ajax.error(function(error){
+                next(source404.replace('$URL$',urlDecoder.toRealDelimiters(decodedUrl)));
+            });
         }
     };
 
